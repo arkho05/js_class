60a00c74f7386eeb68a81ee9e96accb5483e637e
arkho05 <123075443+arkho05@users.noreply.github.com>
2023-10-05 21:34:55 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict";   // trear all js code as newer version 
+
+// alert(3 + 3) // we are useig nodejs, not browser
+
+console.log(3+3); //code readability should be high 
+
+
+// node 01_basics/02_dataTypes.js//
+
+console.log("arkho")
+
+let name = "Arkho"
+let age = 23
+let isloggedIn = false 
+
+
+
+// number => 2 to power 53 
+//bigint 
+// String => ""
+// boolean => standalone value 
+// umdifined => 
+//symbol => unique 
+
+// object 
+
+console.log(typeof age);
+console.log (typeof name);
+console .log ( typeof null);
+console.log(typeof Object);
---
+++ 01_basics/variable.js
@@ -0,0 +1,20 @@
+const accountId = 145533
+let accountEmail ="ararkho@gmail.com"
+var accountPassword = "12345"
+accountcity = "sylhet"
+let accountstate
+
+//accountid = 2 // not allowed 
+// node 01_basics/variable.js// 
+
+accountEmail = "ar@ar.com"
+accountPassword = "45678"
+accountcity = "Mauvibazar"
+
+
+/* Prefer not to use var
+because of issue in block and function Scope 
+*/
+
+
+console.table([ accountId , accountEmail , accountPassword, accountcity , accountstate])
