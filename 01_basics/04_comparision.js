3436effe29702fab58db12ee72d0d4e918b5fad0
arkho05 <123075443+arkho05@users.noreply.github.com>
2023-10-06 22:40:43 +0000

    comparision done

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,28 @@
+// console.log(2 > 1);
+// console.log( 2 >= 1)
+// console.log( 2 < 1);
+// console.log( 2 == 1);
+// console.log( 2 != 1); 
+
+//node 01_basics/04_comparision.js
+
+// console.log("2" > 1);
+// console.log( "02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log (null >= 0);
+
+// console.log (undefined == 0);
+// console.log ( undefined > 0);
+// console.log( undefined < 0);
+
+// don't use code 
+
+//===
+
+
+console.log( "2" === 2);
+
+console.log( 2 === 2);
+
