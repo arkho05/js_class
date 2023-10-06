30aa3cce15934c065f376f77a11ff4dc1ab8020b
arkho05 <123075443+arkho05@users.noreply.github.com>
2023-10-06 22:05:11 +0000

    operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -32,6 +32,54 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof (stringNumber))
+// console.log(stringNumber)
+// console.log(typeof (stringNumber))
+
+// *********************** operations *****************
+
+let value  = 3
+let negvalue = -value
+//console.log(negvalue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**4);
+// console.log(8/2);
+// console.log(2%3);
+
+let str1 = 2 , str2 = 5
+let str3 = str1 + str2
+
+
+// console.log(str3);
+
+
+// console.log( "1" + 2);
+// console.log( 1 + "2");
+// console.log( "1" + 2 + 2);
+// console.log( 1 + 2 + "2");
+
+//console.log( (2 + 1) * 4 % 4 );
+
+// console.log(+true);
+// console.log(+"");
+
+
+let  num1, num2, num3
+
+num1 = num2 = num3 = 2+2; //don't use
+
+let gameCounter = 100
+//gameCounter++;
+++gameCounter;
+console.log(gameCounter);
+
+
+//link to study 
+//mdn
+
+
+
 
