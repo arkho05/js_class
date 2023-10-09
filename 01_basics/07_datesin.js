825f29a754d1fd027162188655226bafefafb9f8
arkho05 <123075443+arkho05@users.noreply.github.com>
2023-10-09 23:13:57 +0000

    date_cls_half_compleated

---
+++ 01_basics/07_datesin.js
@@ -0,0 +1,29 @@
+//Dates
+// node 01_basics/07_datesin.js
+
+// let myDate = new Date();
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof(myDate));
+
+// let myCreatedDate = new Date("2023-04-23");
+// console.log(myCreatedDate.toLocaleString());
+
+// let myCreatedDate = new Date(23,0,23,10, 30,);
+// console.log(myCreatedDate.toLocaleString());
+
+let myCreatedDate = new Date("5-12-23");
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now();
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+
+
+console.log(Math.floor(Date.now()/1000));
+
+
+
