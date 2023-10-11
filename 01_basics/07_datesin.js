bf4022955fa4ca9e5c1c82580c9f200d1670cb05
arkho05 <123075443+arkho05@users.noreply.github.com>
2023-10-11 21:17:04 +0000

    data_time done

--- 01_basics/07_datesin.js
+++ 01_basics/07_datesin.js
@@ -1,12 +1,11 @@
 //Dates
 // node 01_basics/07_datesin.js
 
-// let myDate = new Date();
 // console.log(myDate);
 // console.log(myDate.toString());
 // console.log(myDate.toISOString());
 // console.log(myDate.toLocaleString());
-// console.log(typeof(myDate));
+// console.log(typeof(myDate)); //object
 
 // let myCreatedDate = new Date("2023-04-23");
 // console.log(myCreatedDate.toLocaleString());
@@ -21,9 +20,17 @@ let myTimeStamp = Date.now();
 
 // console.log(myTimeStamp);
 // console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
 
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() +1);
+console.log(newDate.getDay());
 
-console.log(Math.floor(Date.now()/1000));
+// `${newDate.getDay} and the time`;
 
+newDate.toLocaleString('default',{
+    weekday : "long"
+});
 
 
