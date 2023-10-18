8ade0b88be8bf3124ff5dd0116a8fe827a96ce98
arkho05 <123075443+arkho05@users.noreply.github.com>
2023-10-18 22:47:11 +0000

    object 2nd part done

---
+++ 02_basics/04_object.js
@@ -0,0 +1,69 @@
+//const tinderUser = new Object() //singleton_object 
+const tinderUser ={} ; //NON-singleton_object
+
+//node 02_basics/04_object.js
+
+
+tinderUser.id = "123abc";
+tinderUser.name = "Arkho";
+tinderUser.isloggedIn = false;
+
+// console.log(tinderUser);
+
+const  regularUser = {
+    emaill : "ararkho@gmail.com",
+    fullname : {
+        userfullname : {
+            firstname : "Arkho",
+            lastname : "deb",
+         
+        }
+    }
+    
+}
+
+// console.log(regularUser);
+
+// console.log(regularUser.fullname.userfullname);
+
+const obj1 = { 1 : "a", 2 : "b"};
+const obj2 = { 3 : "a", 4 : "b"};
+const obj4 = { 5 : "a", 6 : "c"};
+
+// const obj3 = {obj1, obj2};
+// const obj3 = Object.assign({}, obj1, obj2, obj4);
+
+const obj3 = {...obj1, ...obj2, ...obj4};
+
+// console.log(obj3);
+const user = [
+    {  id : "arkho",
+       emaill: "a@gmail.com"
+
+    },
+    {
+
+    },
+    {
+
+    }
+
+]
+
+user[1].user
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+
+console.log(Object.values(tinderUser));
+
+console.log(Object.entries(tinderUser)); //convert to array
+
+console.log(tinderUser.hasOwnProperty('isloggedIn'));
+
+
+
+
+
+
+
