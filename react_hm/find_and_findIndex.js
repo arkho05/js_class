19744c08a66ce7d8fadfe58ab194ecc16df66aea
arkho05 <123075443+arkho05@users.noreply.github.com>
2023-11-26 00:48:05 +0000

    findIndex_done

--- react_hm/find_and_findIndex.js
+++ react_hm/find_and_findIndex.js
@@ -27,3 +27,32 @@ const islargeNumber = (element) => element> 13;
 console.log(array1.findIndex(islargeNumber));
 
 
+
+function isPrime(element) {
+    if (element % 2 === 0 || element < 2) {
+      return false;
+    }
+    for (let factor = 3; factor <= Math.sqrt(element); factor += 2) {
+      if (element % factor === 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+console.log([4, 6, 8, 9, 12].findIndex(isPrime))
+
+console.log([4, 6, 7, 9, 12].findIndex(isPrime))
+
+
+const arrayLike = { 
+    length : 3,
+    "-1" : 0.1, // ingore by finfIndex () -1 < 0
+    0 : 2,
+    1: 7.3,
+    2:4,
+
+ }
+
+
+ console.log(Array.prototype.findIndex.call(arrayLike, (x) => !Number.isInteger(x)))
