3a33a6b65789e31be8c5e1f68bcc47b0399fdc7e
arkho05 <123075443+arkho05@users.noreply.github.com>
2023-12-29 22:38:28 +0000

    for_each_loop

---
+++ 05_iterations/six.js
@@ -0,0 +1,26 @@
+//node 05_iterations/six.js
+
+const coding = [ "js", "ruby" , "java", "python", "cpp"]
+
+
+// const values = coding.forEach( (item) => {
+//     // console.log(item);
+//     return item
+// })
+
+
+// console.log(values);
+
+
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9,10]
+
+// const newNums = myNums.filter( (num) => {
+//     return num>4
+// })
+
+
+
+const newNums = my
+
+console.log(newNums);
