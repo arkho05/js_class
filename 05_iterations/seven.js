28e61e8c8e503c98d9526785d1a0747cae56415c
arkho05 <123075443+arkho05@users.noreply.github.com>
2023-12-31 18:41:17 +0000

    reduce_add_function

--- 05_iterations/seven.js
+++ 05_iterations/seven.js
@@ -25,4 +25,30 @@ const number = [1, 2, 3, 4, 5]
 
 const doubled = number.map( (item) => item * 2);
 
-console.log(doubled);
+// console.log(doubled);
+
+
+const numbers = [1, 2, 3, 4, 5, 6];
+
+const evens = numbers.filter(item => item % 2 === 0); 
+
+// console.log(evens);
+
+// const students = [
+//     {name : "A", grade: 96},
+//     {name : "B", grade: 92},
+//     {name : "C", grade: 84},
+//     {name : "D", grade: 65},
+//     {name : "E", grade: 90}
+// ];
+
+// const studenGrades = students.filter(student => student.grade >= 90);
+// return studenGrades;
+// // console.log(studenGrades);
+
+
+const num = [1, 2, 3, 4, 5, 6 ]
+
+const sum = num.reduce( function (result, item) {return result + item} );
+
+console.log(sum);
