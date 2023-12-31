0a2844e01cbe0241cdb3ac51ffb56199649f89a2
arkho05 <123075443+arkho05@users.noreply.github.com>
2023-12-31 18:10:37 +0000

    filter_map_reduce_

--- 05_iterations/seven.js
+++ 05_iterations/seven.js
@@ -9,4 +9,20 @@ const newNums = myNumber.
                         map((num) => num * 10)
                         .map( (num) => num + 1 )
                         .filter( (num) => num >= 40 )
-console.log(newNums);
+// console.log(newNums);
+
+
+
+
+// const numbers = [3, 12, 54, 12, 4, 4, 3, 12, 16];
+
+// const filteredNumbers = numbers.filter( (number, index) => numbers.indexOf(number) === index);
+
+// console.log(filteredNumbers);
+
+
+const number = [1, 2, 3, 4, 5]
+
+const doubled = number.map( (item) => item * 2);
+
+console.log(doubled);
